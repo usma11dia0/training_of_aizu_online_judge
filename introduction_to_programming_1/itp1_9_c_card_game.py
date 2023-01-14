2984ccd2d1faf6b34138a17efe99075a05e5c891
usma11dia0 <usma11dia0@gmail.com>
2023-01-14 16:40:10 +0900

    add_itp1_c_card_game

--- introduction_to_programming_1/itp1_9_c_card_game.py
+++ introduction_to_programming_1/itp1_9_c_card_game.py
@@ -1,26 +1,37 @@
-# problem: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_9_B&lang=ja
-
-# 関数定義
-def shuffle(card: str, ord_num: int) -> str:
-    target = card[:ord_num]
-    shuffled = card[ord_num:]
-    shuffled += target
-    return shuffled
-
+# problem: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_9_C&lang=ja
 
 # 入力データの取得
-target_card = ""
-result = []
-while True:
-    target_card = str(input())
-    if target_card == "-":
-        break
-    m = int(input())
-    for i in range(0, m):
-        h = int(input())
-        target_card = shuffle(target_card, h)
-    result.append(target_card)
+n = int(input())
+taro_cards = [None] * n
+hanako_cards = [None] * n
+for i in range(0, n):
+    taro_cards[i], hanako_cards[i] = input().split()
 
-# 結果の出力
-for r in result:
-    print(r)
+# 各カードを比較して両者の得点を算出
+taro_score = 0
+hanako_score = 0
+for taro_card, hanako_card in zip(taro_cards, hanako_cards):
+    is_calculated = False
+    for i in range(min(len(taro_card), len(hanako_card))):
+        # 先頭の文字列からASCIIコードの大小を比較。大小が確定したらその時点で処理を終了する。
+        if ord(taro_card[i]) < ord(hanako_card[i]):
+            hanako_score += 3
+            is_calculated = True
+            break
+        elif ord(hanako_card[i]) < ord(taro_card[i]):
+            taro_score += 3
+            is_calculated = True
+            break
+
+    # 最後まで比較が続いた時は、文字列の長さで得点を判定する。
+    if is_calculated == False:
+        if len(taro_card) == len(hanako_card):
+            hanako_score += 1
+            taro_score += 1
+        elif len(taro_card) < len(hanako_card):
+            hanako_score += 3
+        elif len(hanako_card) < len(taro_card):
+            taro_score += 3
+
+# 結果を出力
+print(taro_score, hanako_score)
