837538050ba311031e94c414c870e7734ab51836
usma11dia0 <usma11dia0@gmail.com>
2023-01-29 22:32:57 +0900

    amend_itp1_7_d_matrix_multiplication.py

--- itp1_7_d_matrix_multiplication.py
+++ itp1_7_d_matrix_multiplication.py
@@ -1,1 +1,14 @@
-# 方針　zipとリスト内包表記を用いて実装してみる
+n, m, l = map(int, input().split())
+
+A = [list(map(int, input().split())) for _ in range(0, n)]
+B = [list(map(int, input().split())) for _ in range(0, m)]
+
+C = [[0] * l for _ in range(0, n)]
+
+for i, a_i in enumerate(A):
+    # 行列Bを転置して1行ずつ抽出
+    for j, b_j in enumerate(zip(*B)):
+        C[i][j] = sum(a_i_k * b_k_j for a_i_k, b_k_j in zip(a_i, b_j))
+
+for result in C:
+    print(*result)
