48ff5a9982617ed383a73e8b98751d188a9d70bb
usma11dia0 <usma11dia0@gmail.com>
2023-02-23 16:19:50 +0900

    amend_albs1_6_d_minimum_cost_sort

--- introduction_to_algorithms_and_data_structures/albs1_6_d_minimum_cost_sort.py
+++ introduction_to_algorithms_and_data_structures/albs1_6_d_minimum_cost_sort.py
@@ -1,29 +1,40 @@
 # https://blog.ikappio.com/solve-minimum-cost-sort-using-cpp/#:~:text=%E7%B5%90%E6%A7%8B%E3%82%80%E3%81%9A%E3%81%8B%E3%81%97%E3%82%81%E3%80%82-,ALDS1_6_D%3A%20Minimum%20Cost%20Sort%3A%20%E6%9C%80%E5%B0%8F%E3%82%B3%E3%82%B9%E3%83%88%E3%82%BD%E3%83%BC%E3%83%88,%E3%81%8C%E3%81%8B%E3%81%8B%E3%82%8B%E3%80%81%E3%81%A8%E3%81%84%E3%81%86%E6%83%B3%E5%AE%9A%E3%81%A7%E3%81%99%E3%80%82
 
 
-def solve(w_list, sort_list):
+def min_cost_sort(w_list: list, sort_list: list) -> int:
     ans = 0
     min_num = sort_list[0]
     for i in range(len(w_list)):
-        x = sort_list.index(w_list[i])  # w_list[i]がsort_listの何番目に格納されているかを返す。
-        if x == i:  # w_listとsort_listの要素番号が同じ = swapの必要なし。
+        # x:  w_list[i]のソート後のindex
+        x = sort_list.index(w_list[i])
+        if x == i:
             continue
         sum_cost = 0
-        min_loop_n = 10**4 + 1
-        j = i  # whileループのiterator
-        loop_count = 0  # ?
+        min_gnum = 10**4 + 1
+        # j: whileループ内のiterator
+        j = i
+        loop_count = 0
         while True:
-            loop_count += 1
-            sum_cost += w_list[j]
-            min_loop_n = min(min_loop_n, w_list[j])
             if x == i:
                 break
+            # 方法1 : 未ソート要素の連鎖によりグループ分けし、グループの最小値を常に用いて入れ替えを行う
+            # 例：[6,3,4,2,1,5] → [6, 5, 1], [3, 4, 2]とグループ分けを行い、左グループは1/右グループは2を常に用いて入れ替えを行う。
+            loop_count += 1
+            # 最小値 ⇔ w_list[j]の入れ替えの際に発生するw_list[j]側のコストを加算
+            sum_cost += w_list[j]
+            min_gnum = min(min_gnum, w_list[j])
             w_list[x], w_list[j] = w_list[j], w_list[x]
             x = sort_list.index(w_list[j])
-        ans += min(
-            sum_cost + (loop_count - 2) * min_loop_n,
-            sum_cost + min_loop_n + (loop_count + 1) * min_num,
-        )
+
+        # 1 or 2 のうち小さいほうをansへ加算
+        # 方法1の入れ替えコスト = 最小値以外の総和  + (入れ替え回数 - 1) * グループ内の最小値
+        cost_1 = (sum_cost - min_gnum) + (loop_count - 1) * min_gnum
+
+        # 方法2 : グループ内へリストの最小値を加え、リストの最小値を常に用いて入れ替えを行う
+        # 例：[6,3,4,2,1,5] → [6, 5, 1], [3, 4, 2, 1]とグループ分けを行い、それぞれ1を常に用いて入れ替えを行う。
+        cost_2 = sum_cost + min_gnum + (loop_count + 1) * min_num
+
+        ans += min(cost_1, cost_2)
     return ans
 
 
@@ -31,4 +42,4 @@ n = int(input())
 w_list = list(map(int, input().split()))
 sort_list = sorted(w_list)
 
-print(solve(w_list, sort_list))
+print(min_cost_sort(w_list, sort_list))
