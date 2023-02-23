1d1fab31545ac0a5a73095de62da2362a4eb5122
usma11dia0 <usma11dia0@gmail.com>
2023-02-23 17:25:26 +0900

    modified albs1_6_d_minimum_cost_sort

--- introduction_to_algorithms_and_data_structures/albs1_6_d_minimum_cost_sort.py
+++ introduction_to_algorithms_and_data_structures/albs1_6_d_minimum_cost_sort.py
@@ -15,25 +15,31 @@ def min_cost_sort(w_list: list, sort_list: list) -> int:
         j = i
         loop_count = 0
         while True:
-            if x == i:
-                break
             # 方法1 : 未ソート要素の連鎖によりグループ分けし、グループの最小値を常に用いて入れ替えを行う
             # 例：[6,3,4,2,1,5] → [6, 5, 1], [3, 4, 2]とグループ分けを行い、左グループは1/右グループは2を常に用いて入れ替えを行う。
             loop_count += 1
             # 最小値 ⇔ w_list[j]の入れ替えの際に発生するw_list[j]側のコストを加算
             sum_cost += w_list[j]
             min_gnum = min(min_gnum, w_list[j])
+            if x == i:
+                break
             w_list[x], w_list[j] = w_list[j], w_list[x]
             x = sort_list.index(w_list[j])
 
         # 1 or 2 のうち小さいほうをansへ加算
-        # 方法1の入れ替えコスト = 最小値以外の総和  + (入れ替え回数 - 1) * グループ内の最小値
+        # 方法1の入れ替えコスト = min_gnum以外の総和  + (入れ替え回数 - 1) * min_gnum
+        # (入れ替え回数 - 1) : 最後の1→6のループは入れ替え回数に含めないため-1にする。
         cost_1 = (sum_cost - min_gnum) + (loop_count - 1) * min_gnum
 
         # 方法2 : グループ内へリストの最小値を加え、リストの最小値を常に用いて入れ替えを行う
-        # 例：[6,3,4,2,1,5] → [6, 5, 1], [3, 4, 2, 1]とグループ分けを行い、それぞれ1を常に用いて入れ替えを行う。
-        cost_2 = sum_cost + min_gnum + (loop_count + 1) * min_num
-
+        # 例：[6,3,4,2,1,5] → [6, 5, 1, 1], [3, 4, 2, 1]とグループ分けを行い、それぞれ1を常に用いて入れ替えを行う。
+        # 方法2の入れ替えコスト = min_gnum以外の総和  + (入れ替え回数 - 1) * min_num + (min_gnumとmin_numの入れ替えコスト) * 2
+        # (min_gnumとmin_numの入れ替えコスト) * 2: リストの最小値をグループへ加える/除くための入れ替え
+        cost_2 = (
+            (sum_cost - min_gnum)
+            + (loop_count - 1) * min_num
+            + (min_gnum + min_num) * 2
+        )
         ans += min(cost_1, cost_2)
     return ans
 
