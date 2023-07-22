717204e950057d615abc70efb1b6c9b0a2a9c5c3
usma11dia0 <usma11dia0@gmail.com>
2023-07-22 17:05:44 +0900

    add node class to albs1_8_d_treap

--- introduction_to_algorithms_and_data_structures/albs1_8_d_treap.py
+++ introduction_to_algorithms_and_data_structures/albs1_8_d_treap.py
@@ -1,142 +1,34 @@
 # https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=7093165#1
 
+# 2分探索木の特徴　参考：アルゴリズム図鑑(アプリ)
+# 1. 全てのノードは、そのノードの左部分木に含まれるどの数よりも大きくなる
+# 2. 全てのノードは、そのノードの右部分木に含まれるどの数よりも小さくなる
+
+# 2分探査木にてノードを削除する場合
+# 子ノードが一つもない場合：そのまま削除
+# 子ノードが一つの場合：子ノードを削除ノードの場所へ移し替える
+# 子ノードが二つの場合：削除ノードの左部分木から最大ノードを探索し、削除ノードの場所へ移し替える。
+
 from collections import deque
 import sys
 
 
+#二分探索木を実装
 class Node:
-    def __init__(self, key: int, priority: int) -> None:
+    def __init__(self, key: int) -> None:
         self.key = key
-        self.priority = priority
-        self.left = None
         self.right = None
-        self.parent = None
-
+        self.left = None
 
 class Treap:
-    def __init__(self) -> None:
-        self.root = None
+    def __init__(self, root: Node) -> Node:
+        self.root = root
 
-    def _rightRotate(self, t: Node) -> Node:
-        s = Node(None, None)
-        s = t.left
-        t.left = s.right
-        s.right = t
-        return s
-
-    def _leftRotate(self, t: Node) -> Node:
-        s = Node(None, None)
-        s = t.right
-        t.right = s.left
-        s.left = t
-        return s
-
-    def insert(self, key: int, priority: int) -> None:
-        def _insert(node: Node, key: int, priority: int) -> Node:
-            if node == None:
-                return Node(key, priority)
-            if key == node.key:
-                return node
-
-            if key < node.key:
-                node.left = _insert(node.left, key, priority)
-                if node.priority < node.left.priority:
-                    node = self._rightRotate(node)
-            else:
-                node.right = _insert(node.right, key, priority)
-                if node.priority < node.right.priority:
-                    node = self._leftRotate(node)
-            return node
-
-        self.root = _insert(self.root, key, priority)
-
-    def find(self, key: int) -> Node:
-        def _find(node: Node, key: int) -> Node:
-            if node == None:
-                return None
-            if key == node.key:
-                return node
-            elif key < node.key:
-                return _find(node.left, key)
-            elif key > node.key:
-                return _find(node.right, key)
-
-        return _find(self.root, key)
-
-    def delete(self, node: Node, key: int) -> None:
-        if node == None:
-            return None
-        if key < node.key:
-            node.left = _delete(node.left, key)
-        elif key > node.key:
-            node.right = _delete(node.right, key)
-        else:
-            return _delete(node, key)
-
-        def _delete(node: Node, key: int) -> None:
-            delete_node = node
-            if delete_node.left is None and delete_node.right is None:
-                return None
-            elif delete_node.left is None:
-                delete_node = self._leftRotate(delete_node)
-            elif delete_node.right is None:
-                delete_node = self._rightRotate(delete_node)
-            else:
-                if delete_node.left.priority > delete_node.right.priority:
-                    delete_node = self._rightRotate(delete_node)
-                else:
-                    delete_node = self._leftRotate(delete_node)
-            return self.delete(delete_node, key)
+    def insert(self, z: int) -> None:
+        # zをノード化
+        z = Node(z)
+        # Treapの根
+        root_treap = self.root 
+        
 
 
-# 深さ優先探索(リスト生成)
-def preorder(node: Node, queue: deque) -> list:
-    if node.left != None:
-        preorder(node.left, queue)
-    queue.append(node)
-    if node.right != None:
-        preorder(node.right, queue)
-
-
-# 探索順を全て出力
-def print_order_all(T: Treap) -> None:
-    # 深さ優先探索(先行順巡回)
-    def _print_preorder(node: Node) -> None:
-        print(f" {node.key}", end="")
-        if node.left != None:
-            _print_preorder(node.left)
-        if node.right != None:
-            _print_preorder(node.right)
-
-    # 深さ優先探索(中間順巡回)
-    def _print_inorder(node: Node) -> None:
-        if node.left != None:
-            _print_inorder(node.left)
-        print(f" {node.key}", end="")
-        if node.right != None:
-            _print_inorder(node.right)
-
-    # 結果の出力
-    _print_inorder(T.root)
-    print()
-    _print_preorder(T.root)
-    print()
-
-
-m = int(input())
-command_list = [list(input().split()) for _ in range(0, m)]
-
-T = Treap()
-
-for command in command_list:
-    if command[0] == "insert":
-        T.insert(int(command[1]), int(command[2]))
-    elif command[0] == "find":
-        if T.find(int(command[1])) != None:
-            print("yes")
-        else:
-            print("no")
-    elif command[0] == "delete":
-        T.delete(int(command[1]))
-    elif command[0] == "print":
-        print_order_all(T)
