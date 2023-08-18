c7bd59d39276f1e510f43f200ed1bfdd269a48a0
usma11dia0 <usma11dia0@gmail.com>
2023-08-19 00:24:50 +0900

    complete albs1_12_a_minimum_spanning_tree.py

--- introduction_to_algorithms_and_data_structures/albs1_12_a_minimum_spanning_tree.py
+++ introduction_to_algorithms_and_data_structures/albs1_12_a_minimum_spanning_tree.py
@@ -3,6 +3,8 @@
 # クラスカルのアルゴリズム：どの辺も選択していない状態からスタートし、重みが小さい辺から順番に追加していって最終的に全域木を作る方法 (貪欲法)
 # ※ただし閉路となる場合は追加しない(木の定理を満たさないため)
 
+# 各辺を追加する際にunion-findの判定を用いて、かつ条件を満たしたものをunion-findの木に追加することで、クラスカルのアルゴリズムを実装。
+
 # 教訓
 # unite()を実装するために必要なこと
 # １．頂点aのrootと頂点bのrootを調べる。
@@ -15,19 +17,19 @@
 # Union-find木を定義
 class UnionFind:
     # 初期化
-    def __init__(self, n):
+    def __init__(self, n: int) -> None:
         self.n = n  # Union-Find木の頂点の数
         self.par = [-1] * (n + 1)  # 各頂点の親は初期状態では自分自身(親が自分自身≒親無しの場合-1) ※1-indexedで実装
         self.size = [1] * (n + 1)  # 各頂点に紐づくノードの数は、初期状態では1 ※1-indexedで実装
 
     # 頂点xのrootを調べる関数
-    def root(self, x):
+    def root(self, x: int) -> int:
         while self.par[x] != -1:
             x = self.par[x]
         return x
 
     # 頂点uと頂点vを連結させる関数
-    def unite(self, u, v):
+    def unite(self, u: int, v: int) -> None:
         # 頂点uとvのrootを調べ、rootが既に同一でないかどうかを調べる。
         rootu = self.root(u)
         rootv = self.root(v)
@@ -42,20 +44,29 @@ class UnionFind:
                 self.size[rootu] += self.size[rootv]  # 連結された新たな木(頂点uのルート)のサイズを更新する。
 
     # 頂点uと頂点vが同じ連結成分に属するかどうかを調べる関数
-    def same(self, u, v):
+    def same(self, u: int, v: int) -> bool:
         return self.root(u) == self.root(v)
 
 
 # 入力データの取得
-N, M = map(int, input().split())
-edges = [list(map(int, input().split())) for _ in range(0, M)]
+n = int(input())
+edges = []
+cnt = 0
+
+# 隣接行列より隣接リストを作成
+while cnt < n:
+    a_i = list(map(int, input().split()))
+    for j in range(cnt + 1, n):
+        if a_i[j] != -1:
+            edges.append([cnt + 1, j + 1, a_i[j]])
+    cnt += 1
 
 # 辺(x[2])を長さの小さい順にsortする。
 edges.sort(key=lambda x: x[2])  # 比較を行う前にリストの各要素に対して呼び出される関数を指定。
 # リストの各要素xとして、x[2]を指定している。
 
 # 最小全域木を求める
-uf = UnionFind(N)
+uf = UnionFind(n)
 answer = 0
 for a, b, c in edges:
     if not uf.same(a, b):  # 頂点a,bがまだ同じ連結成分に属していない場合
